e1a8e3661dd81903e63a39539c1cb97f32e836a7
ignacio-molinam <ignacio.molina@genstudents.org>
2024-11-26 15:35:16 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+
+nombre = input("ingresa tu nombre")
+apellido = input("ingresa tu apellido")
+ciudad = input("ingresa tu ciudad")
+print (f"hola,{nombre}{apellido}. Bienvenido a nuestra app. seleccionaste la ciudad {ciudad}")
